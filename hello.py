982dd650e8ed6a819b5eac79650aaa22f4736b14
Alexander Bien <alexanderpbien@gmail.com>
2019-07-16 08:27:59 -0500

    Cool way to format print (in hello.txt)

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ print("Hello World")
 x = "hello fahad"
 y = "and hello Alex"
 print(f'{x} {y}')
+
+#i found this really cool way to format the print in another textbook
+x = 'alex'
+y = 'fahad'
+z = 'jeni'
+print("Hello, my name is {0} and this is my friends {1} and {2}".format(x,y,z) )
